3e0bb77d6607511229da7d84331227b1a63a5df1
ykuruta <yasukokurata@gmail.com>
2018-12-27 00:39:16 -0500

    added a feature in T-shirt info

--- js/app.js
+++ js/app.js
@@ -1,2 +1,35 @@
 // Focus on the first text field
 $('#name').focus();
+
+
+// "Your Job Role" field reveals when the "Other" option is selected
+$(document).ready(function() {
+ $("#title").change(function() {
+  if($(this).val() == "other") {
+   $("#job").show();
+  } else {
+   $("#job").hide();
+  }
+ });
+ $("#job").hide();
+});
+
+
+// Only display the color options that match the design selected in the "Design" menu.
+$(document).ready(function() {
+  $('#design').change(function() {
+    if ($(this).val() === "js puns") {
+      $('#color').children('option').hide();
+      $('#color').children("option[value='cornflowerblue']").show();
+      $('#color').children("option[value='darkslategrey']").show();
+      $('#color').children("option[value='gold']").show();
+    } else if ($(this).val() === "heart js") {
+      $('#color').children('option').hide();
+      $('#color').children("option[value='tomato']").show();
+      $('#color').children("option[value='steelblue']").show();
+      $('#color').children("option[value='dimgrey']").show();
+    } else {
+      $('#color').children('option').show();
+    }
+  })
+})
