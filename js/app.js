1b82f7b405b0217c4ec3c804e2f971309e6ca8a6
ykuruta <yasukokurata@gmail.com>
2018-12-27 18:32:39 -0500

    disable to select the activity at the same time

--- js/app.js
+++ js/app.js
@@ -31,5 +31,56 @@ $(document).ready(function() {
     } else {
       $('#color').children('option').show();
     }
-  })
-})
+  });
+});
+
+
+// ”Register for Activities” section
+const jsFrameworks = $('input[name="js-frameworks"]');
+const express = $('input[name="express"]');
+const jsLibs = $('input[name="js-libs"]');
+const node = $('input[name="node"]');
+
+$(document).ready(function() {
+  $('input').on('click', function() {
+    if (jsFrameworks.is(':checked')) {
+      $('input[name="express"]').prop('checked', false); //remove check
+      $('input[name="express"]').prop('disabled', true); //disable input
+    } else {
+      $('input[name="express"]').removeAttr('disabled'); //enable input
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('input').on('click', function() {
+    if (express.is(':checked')) {
+      $('input[name="js-frameworks"]').prop('checked', false); //remove check
+      $('input[name="js-frameworks"]').prop('disabled', true); //disable input
+    } else {
+      $('input[name="js-frameworks"]').removeAttr('disabled'); //enable input
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('input').on('click', function() {
+    if (jsLibs.is(':checked')) {
+      $('input[name="node"]').prop('checked', false); //remove check
+      $('input[name="node"]').prop('disabled', true); //disable input
+    } else {
+      $('input[name="node"]').removeAttr('disabled'); //enable input
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('input').on('click', function() {
+    if (node.is(':checked')) {
+      $('input[name="js-libs"]').prop('checked', false); //remove check
+      $('input[name="js-libs"]').prop('disabled', true); //disable input
+    } else {
+      $('input[name="js-libs"]').removeAttr('disabled'); //enable input
+    }
+  });
+});
